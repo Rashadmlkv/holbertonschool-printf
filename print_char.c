975ac0c33ecaab81b48988caa548fe035f62488c
Rashadmlkv <resadmlkv@gmail.com>
2023-11-27 06:02:03 -0800

    putchar print_char and print_string

---
+++ print_char.c
@@ -0,0 +1,12 @@
+/**
+ * print_char - print character
+ * @r: leng
+ * @c: character
+ * Return: leng
+ */
+int print_char(unsigned int r, char c)
+{
+	r++;
+	_putchar(c);
+	return (r);
+}
---
+++ print_string.c
@@ -0,0 +1,18 @@
+/**
+ * print_string - print string
+ * @r: leng
+ * @s: string
+ * Return: leng
+ */
+int print_string(unsigned int r, char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		_putchar(*(s + i));
+		i++;
+		r++;
+	}
+	return (r);
+}
