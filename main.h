60ddd205da148c56c0682cf2a8da69d42d931217
root <isa.quluyev.wip@gmail.com>
2023-11-27 05:39:39 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int print_char(unsigned int r, char c);
+int print_string(unsigned int r, char *s);
+int _printf(const char *format, ...);
+#endif
