242e5cd2baf4e13c54ce4db951375d00841fddb3
Rashadmlkv <resadmlkv@gmail.com>
2023-11-28 05:04:12 -0800

    added new function prototype

--- main.h
+++ main.h
@@ -6,4 +6,5 @@ int _putchar(char c);
 int print_char(unsigned int r, char c);
 int print_string(unsigned int r, char *s);
 int _printf(const char *format, ...);
+int print_decimal(unsigned int r, int s);
 #endif
